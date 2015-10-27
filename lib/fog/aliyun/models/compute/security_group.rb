55934b09937b11be4e3c2d7a87521746d82432a7
sx-h9030 <sx-hanyt@dtdream.com>
2015-10-27 11:25:16 +0800

    1. use each instead of for..in 2. use 2 spaces instead of tab

--- lib/fog/aliyun/models/compute/security_group.rb
+++ lib/fog/aliyun/models/compute/security_group.rb
@@ -4,107 +4,83 @@ module Fog
   module Compute
     class Aliyun
       class SecurityGroup < Fog::Model
-        # {"SecurityGroup"=>[{"CreationTime"=>"2015-08-10T03:21:02Z", 
-        #   "SecurityGroupId"=>"sg-25mexinlu", "Description"=>"", "SecurityGroupName"=>"bosh", 
-        #   "VpcId"=>"vpc-25mj6mguq"}}
-
-        identity  :id,                    :aliases =>"SecurityGroupId"
-
-        attribute :name,                  :aliases => "SecurityGroupName"
-        attribute :description,           :aliases => "Description"
-        attribute :created_at,            :aliases => "CreationTime"
-        attribute :vpc_id,                :aliases => "VpcId"
+        identity  :id,           :aliases =>"SecurityGroupId"
+        attribute :name,         :aliases => "SecurityGroupName"
+        attribute :description,  :aliases => "Description"
+        attribute :created_at,   :aliases => "CreationTime"
+        attribute :vpc_id,       :aliases => "VpcId"
 
         def vpc
-            requires :vpc_id
-            $vpc=Fog::Compute::Aliyun::Vpcs.new(:service=>service).all('vpcId'=>vpc_id)[0]
+          requires :vpc_id
+          $vpc=Fog::Compute::Aliyun::Vpcs.new(:service=>service).all('vpcId'=>vpc_id)[0]
         end
 
         def security_group_rules
-            requires :id
-            Fog::Compute::Aliyun::SecurityGroupRules.new(:service=>service).get(id)
+          requires :id
+          Fog::Compute::Aliyun::SecurityGroupRules.new(:service=>service).get(id)
         end
 
         def save(options={})
-            options[:vpcId] = vpc_id if vpc_id
-            options[:name] = name if name
-            options[:description] = description if description
-            data = Fog::JSON.decode(service.create_security_group(options).body)
-            true
+          options[:vpcId] = vpc_id if vpc_id
+          options[:name] = name if name
+          options[:description] = description if description
+          data = Fog::JSON.decode(service.create_security_group(options).body)
+          true
         end
 
-
         def destroy
-            requires :id
-            service.delete_security_group(id)
-            true
+          requires :id
+          service.delete_security_group(id)
+          true
         end
 
         def authorize_seurity_group_sg_rule(group_id,direction="ingress",options={})
-            requires :id
-            if direction=="egress"
-                service.create_security_group_egress_sg_rule(id,group_id,options)
-            else
-                service.create_security_group_sg_rule(id,group_id,options)
-            end
-                    
-            true
+          requires :id
+          if direction=="egress"
+            service.create_security_group_egress_sg_rule(id,group_id,options)
+          else
+            service.create_security_group_sg_rule(id,group_id,options)
+          end
+          true
         end
 
         def authorize_security_group_ip_rule(cidr_ip,direction="ingress",options={})
-            requires :id
-            nic_type=options[:nic_type]
-            unless nic_type
-                nic_type="internet"
-            end
-
-            if direction=="egress"
-                service.create_security_group_egress_ip_rule(id,cidr_ip,nic_type,options)
-            else
-                service.create_security_group_ip_rule(id,cidr_ip,nic_type,options)
-            end
-                    
-            true
+          requires :id
+          nic_type=options[:nic_type]
+          unless nic_type
+            nic_type="internet"
+          end
+          if direction=="egress"
+            service.create_security_group_egress_ip_rule(id,cidr_ip,nic_type,options)
+          else
+            service.create_security_group_ip_rule(id,cidr_ip,nic_type,options)
+          end
+          true
         end
 
         def revoke_seurity_group_sg_rule(group_id,direction="ingress",options={})
-            requires :id
-            if direction=="egress"
-                service.delete_security_group_egress_sg_rule(id,group_id,options)
-            else
-                service.delete_security_group_sg_rule(id,group_id,options)
-            end
-                    
-            true
+          requires :id
+          if direction=="egress"
+            service.delete_security_group_egress_sg_rule(id,group_id,options)
+          else
+            service.delete_security_group_sg_rule(id,group_id,options)
+          end
+          true
         end
 
         def revoke_security_group_ip_rule(cidr_ip,direction="ingress",options={})
-            requires :id
-            nic_type=options[:nic_type]
-            unless nic_type
-                nic_type="internet"
-            end
-
-            if direction=="egress"
-                service.delete_security_group_egress_ip_rule(id,cidr_ip,nic_type,options)
-            else
-                service.delete_security_group_ip_rule(id,cidr_ip,nic_type,options)
-            end
-                    
-            true
+          requires :id
+          nic_type=options[:nic_type]
+          unless nic_type
+            nic_type="internet"
+          end
+          if direction=="egress"
+            service.delete_security_group_egress_ip_rule(id,cidr_ip,nic_type,options)
+          else
+            service.delete_security_group_ip_rule(id,cidr_ip,nic_type,options)
+          end
+          true
         end
-
-        # def create_security_group_rule(min, max, ip_protocol = "tcp", cidr = "0.0.0.0/0", group_id = nil)
-        #   Fog::Logger.deprecation('#create_security_group_rule is deprecated. Use the Fog::Compute::Openstack::SecurityGroupRules collection to create new rules.')
-        #   requires :id
-        #   service.create_security_group_rule(id, ip_protocol, min, max, cidr, group_id)
-        # end
-
-        # def delete_security_group_rule(rule_id)
-        #   Fog::Logger.deprecation('#create_security_group_rule is deprecated. Use the Fog::Compute::Openstack::SecurityGroupRule objects to destroy rules.')
-        #   service.delete_security_group_rule(rule_id)
-        #   true
-        # end
       end
     end
   end
--- lib/fog/aliyun/models/compute/security_group_rules.rb
+++ lib/fog/aliyun/models/compute/security_group_rules.rb
@@ -11,17 +11,12 @@ module Fog
         def get(security_group_id,options={})
           data = Fog::JSON.decode(service.list_security_group_rules(security_group_id, options).body)
           self.security_group_id = data["SecurityGroupId"]
-          data = data["Permissions"]["Permission"]
-          for i in data
-            i["SecurityGroupId"] = self.security_group_id
+          permissions = data["Permissions"]["Permission"]
+          permissions.each do |permission|
+            permission["SecurityGroupId"] = self.security_group_id
           end
-          load(data)
+          load(permissions)
         end
-        
-        # def get(security_group_id, options={})
-          
-        # end
-        
       end
     end
   end
