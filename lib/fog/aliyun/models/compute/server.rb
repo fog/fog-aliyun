fecd1c7901e12ae18364d38b90d03f3cddce2d80
Amit Upadhye <upadhyeammit@gmail.com>
2019-05-24 19:20:33 +0530

    add vswitch_id param to server

--- lib/fog/aliyun/models/compute/server.rb
+++ lib/fog/aliyun/models/compute/server.rb
@@ -34,6 +34,7 @@ module Fog
         attribute :charge_type, aliases: 'InstanceChargeType'
         attribute :operation_locks, aliases: 'OperationLocks'
         attribute :expired_at, aliases: 'ExpiredTime'
+        attribute :vswitch_id, aliases: 'VSwitchId'
 
         def image
           requires :image_id
