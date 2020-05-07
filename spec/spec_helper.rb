9916ef6305a88e6839bd8a89b37933f96ea851ea
i068165 <dmitry.bakaleinik@sap.com>
2020-05-07 16:21:46 +0300

    Implement basic integration tests Add codecoverage support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'test'
+  end
+end
+
+if !ENV['INTEGRATION']
+  RSpec.configure do |c|
+    c.filter_run_excluding :integration => true
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'fog/aliyun'
