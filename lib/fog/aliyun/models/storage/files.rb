6824e7a5d0bd4bda1e69f41a185ed3f11d776193
zjd <1418408614@qq.com>
2020-07-14 16:51:10 +0800

    fix files all options problem

--- lib/fog/aliyun/models/storage/files.rb
+++ lib/fog/aliyun/models/storage/files.rb
@@ -23,10 +23,10 @@ module Fog
         def all(options = {})
           requires :directory
           options = {
-              'delimiter'   => delimiter,
-              'marker'      => marker,
-              'max-keys'    => max_keys.to_i,
-              'prefix'      => prefix
+              'delimiter': delimiter,
+              'marker': marker,
+              'max-keys': max_keys.to_i,
+              'prefix': prefix
           }.merge!(options)
           options = options.reject {|key,value| value.nil? || value.to_s.empty?}
           merge_attributes(options)
