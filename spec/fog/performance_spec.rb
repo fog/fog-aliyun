2825d9d1f711382eabe1537641878e1aeb7ed492
zjd <1418408614@qq.com>
2020-07-14 14:41:59 +0800

    reconstruct perform test

--- spec/fog/performance_spec.rb
+++ spec/fog/performance_spec.rb
@@ -17,8 +17,9 @@ describe 'Performance tests', :performance => true do
     system("aliyun oss mb oss://#{@conn.aliyun_oss_bucket} > /dev/null")
   end
 
-  it 'Should upload 5 mb file' do
-    content = "A" * 5 * 1024 * 1024
+
+  it 'Should upload 5 mb string' do
+    content="A"*5*1024*1024
     directory = @conn.directories.get(@conn.aliyun_oss_bucket)
     Benchmark.bm(9)  do |benchmark|
       benchmark.report('Upload file time:') do
@@ -29,6 +30,126 @@ describe 'Performance tests', :performance => true do
       end
     end
     @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+  end
+
+  it 'Should upload 5 mb file' do
+    f=File.open("1.txt","w+")
+    content="A"*1024*1024
+    5.times.each do
+      f.write(content)
+    end
+    f.seek(0)
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Upload file time:') do
+        @memReport = MemoryProfiler.report do
+          directory.files.create :key => 'test_dir/test_file1',
+                                 :body => f
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+    f.close
+    File.delete("1.txt")
+  end
+
+  it 'Should upload 10 mb string' do
+    content="A"*10*1024*1024
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Upload file time:') do
+        @memReport = MemoryProfiler.report do
+          directory.files.create :key => 'test_dir/test_file1',
+                                 :body => content
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+  end
+
+  it 'Should upload 10 mb file' do
+    f=File.open("1.txt","w+")
+    content="A"*1024*1024
+    10.times.each { f.write(content) }
+    f.seek(0)
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Upload file time:') do
+        @memReport = MemoryProfiler.report do
+          directory.files.create :key => 'test_dir/test_file1',
+                                 :body => f
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+    f.close
+    File.delete("1.txt")
+  end
+
+  it 'Should upload 50 mb string' do
+    content="A"*50*1024*1024
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Upload file time:') do
+        @memReport = MemoryProfiler.report do
+          directory.files.create :key => 'test_dir/test_file1',
+                                 :body => content
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+  end
+
+  it 'Should upload 50 mb file' do
+    f=File.open("1.txt","w+")
+    content="A"*1024*1024
+    50.times.each{ f.write(content) }
+    f.seek(0)
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Upload file time:') do
+        @memReport = MemoryProfiler.report do
+          directory.files.create :key => 'test_dir/test_file1',
+                                 :body => f
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+    f.close
+    File.delete("1.txt")
+  end
+
+  it 'Should upload 100 mb string' do
+    content="A"*100*1024*1024
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Upload file time:') do
+        @memReport = MemoryProfiler.report do
+          directory.files.create :key => 'test_dir/test_file1',
+                                 :body => content
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+  end
+
+  it 'Should upload 100 mb file' do
+    f=File.open("1.txt","w+")
+    content="A"*1024*1024
+    100.times.each{ f.write(content) }
+    f.seek(0)
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Upload file time:') do
+        @memReport = MemoryProfiler.report do
+          directory.files.create :key => 'test_dir/test_file1',
+                                 :body => f
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+    f.close
+    File.delete("1.txt")
   end
 
   it 'Should download 5 mb file' do
@@ -47,5 +168,58 @@ describe 'Performance tests', :performance => true do
     end
     @memReport.pretty_print :detailed_report => true, :scale_bytes => true
   end
+
+  it 'Should download 10 mb file' do
+    content = "A" * 10 * 1024 * 1024
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    directory.files.create :key => 'test_dir/test_file1',
+                           :body => content
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Download file time:') do
+        @memReport = MemoryProfiler.report do
+          files = directory.files
+          file = files.get('test_dir/test_file1')
+          file.body
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+  end
+
+  it 'Should download 50 mb file' do
+    content="A"*50*1024*1024
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    directory.files.create :key => 'test_dir/test_file1',
+                           :body => content
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Download file time:') do
+        @memReport = MemoryProfiler.report do
+          files = directory.files
+          file = files.get('test_dir/test_file1')
+          file.body
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+  end
+
+  it 'Should download 100 mb file' do
+    content="A"*100*1024*1024
+    directory = @conn.directories.get(@conn.aliyun_oss_bucket)
+    directory.files.create :key => 'test_dir/test_file1',
+                           :body => content
+    Benchmark.bm(9)  do |benchmark|
+      benchmark.report('Download file time:') do
+        @memReport = MemoryProfiler.report do
+          files = directory.files
+          file = files.get('test_dir/test_file1')
+          file.body
+        end
+      end
+    end
+    @memReport.pretty_print :detailed_report => true, :scale_bytes => true
+  end
+
+
 
 end
